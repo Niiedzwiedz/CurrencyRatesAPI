6229e3f6b0f253e97fcce5e6552e8f7f45b7748e
Niiedzwiedz <vinter98@gmail.com>
2021-09-08 22:42:04 +0200

    Parameters mk.1

--- CurrencyRatesAPI/Controllers/CurrencyRatesController.cs
+++ CurrencyRatesAPI/Controllers/CurrencyRatesController.cs
@@ -10,56 +10,100 @@ namespace CurrencyRatesAPI.Controllers
     [Route("api/currencyRates")]
     public class CurrencyRatesController : ControllerBase
     {
-        [HttpGet("test")]
-        public ActionResult<List<CurrencyRate>> GetAll()
+        [HttpGet("parameters")]
+        public ActionResult<List<CurrencyRate>> GetAll([FromBody] SearchParameters parameters)
         {
             //Test String
             //https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.USD.EUR.SP00.A?startPeriod=2021-03-01&endPeriod=2021-03-05
-
             List<CurrencyRate> rates = new List<CurrencyRate>();
-            //Dummy Query
-            var querryString = "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.USD.EUR.SP00.A?startPeriod=2009-05-01&endPeriod=2009-05-31";
+
+            //Assembly "Querry"
+            int i = 0;
+            var querryString = $"https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D";
+            foreach (KeyValuePair<string, string> v in parameters.CurrencyCodes)
+            {
+                if(++i == 1) querryString += $".{v.Key}";
+                else querryString += $"+{v.Key}";
+            }
+            querryString+= $".EUR.SP00.A?startPeriod={parameters.StartDate.ToString("yyyy-MM-dd")}&endPeriod={parameters.EndDate.ToString("yyyy-MM-dd")}";
+            //Console.WriteLine(querryString);
 
             var doc = new XmlDocument();
-            doc.Load(querryString);
-            var nsmgr = new XmlNamespaceManager(doc.NameTable);
-            nsmgr.AddNamespace("message", "http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message");
-            nsmgr.AddNamespace("generic", "http://www.sdmx.org/resources/sdmxml/schemas/v2_1/data/generic");
-            XmlNodeList nodes = doc.DocumentElement.SelectNodes("//message:GenericData/message:DataSet/generic:Series/generic:SeriesKey/generic:Value", nsmgr);
-
-            string from = "???";
-            string to = "???";
-            if (nodes != null)
+            XmlNamespaceManager nsmgr;
+            XmlNodeList nodes;
+            try
             {
-                foreach (XmlNode node in nodes)
-                {
-                    if (node.Attributes["id"].Value == "CURRENCY") to = node.Attributes["value"].Value;
-                    if (node.Attributes["id"].Value == "CURRENCY_DENOM") from = node.Attributes["value"].Value;
-                }
+                doc.Load(querryString);
+
+                nsmgr = new XmlNamespaceManager(doc.NameTable);
+                nsmgr.AddNamespace("message", "http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message");
+                nsmgr.AddNamespace("generic", "http://www.sdmx.org/resources/sdmxml/schemas/v2_1/data/generic");
+
+                //Get series
+                nodes = doc.DocumentElement.SelectNodes("//message:GenericData/message:DataSet/generic:Series", nsmgr);
+            }
+            catch
+            {
+                return BadRequest();
             }
 
-            nodes = doc.DocumentElement.SelectNodes("//message:GenericData/message:DataSet/generic:Series/generic:Obs", nsmgr);
-            if (nodes != null)
+            foreach (XmlNode node in nodes)
             {
-                foreach (XmlNode node in nodes)
-                {
+                string from = "";
+                string to = "";
 
-                    DateTime date = new DateTime(1970, 01, 01);
+                //Get FROM and TO codes
+                foreach (XmlNode keys in node.SelectNodes("generic:SeriesKey/generic:Value", nsmgr))
+                {
+                    if (keys.Attributes["id"].Value == "CURRENCY") 
+                        to = keys.Attributes["value"].Value;
+                    if (keys.Attributes["id"].Value == "CURRENCY_DENOM") 
+                        from = keys.Attributes["value"].Value;
+                }
+
+                foreach (XmlNode dailesNode in node.SelectNodes("generic:Obs", nsmgr))
+                {
+                    DateTime date = new DateTime();
                     double value = 0.0;
 
-                    foreach (XmlNode subnode in node.ChildNodes)
+                    //Get Date
+                    var dateNode = dailesNode.SelectSingleNode("generic:ObsDimension", nsmgr);
+                    if (dateNode != null)
                     {
-                        if (subnode.Name == "generic:ObsDimension")
-                            date = DateTime.ParseExact(subnode.Attributes["value"].Value, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                        if (subnode.Name == "generic:ObsValue")
-                        {
-                            if (subnode.Attributes["value"].Value == null || subnode.Attributes["value"].Value.ToString() == "NaN") break;
-                            value = (double)Decimal.Parse(subnode.Attributes["value"].Value, NumberStyles.Float, new CultureInfo("en-Us"));
-                            break;
-                        }
+                        date = DateTime.ParseExact(
+                            dateNode.Attributes["value"].Value, 
+                            "yyyy-MM-dd", 
+                            CultureInfo.InvariantCulture
+                            );
                     }
 
-                    if (value == 0.0) continue;
+                    //Get Rate
+                    var rateNode = dailesNode.SelectSingleNode("generic:ObsValue", nsmgr);
+                    if (rateNode != null)
+                    {
+                        //if Rate Record == NaN skip
+                        if (
+                            rateNode.Attributes["value"].Value == null || 
+                            rateNode.Attributes["value"].Value.ToString() == "NaN"
+                            ) continue;
+
+                        value = (double)Decimal.Parse
+                            (
+                            rateNode.Attributes["value"].Value, 
+                            NumberStyles.Float, 
+                            new CultureInfo("en-Us")
+                            );
+                    }
+
+                    //Fine Check
+                    if (
+                        value == 0.0            || 
+                        date == new DateTime()  || 
+                        from.Length == 0        || 
+                        to.Length == 0
+                        ) continue;
+
+                    //Save
                     var rate = new CurrencyRate()
                     {
                         From = from,
@@ -67,9 +111,11 @@ namespace CurrencyRatesAPI.Controllers
                         Value = value,
                         Date = date
                     };
+
                     rates.Add(rate);
                 }
             }
+
             return Ok(rates);
         }
     }
---
+++ CurrencyRatesAPI/Data/SearchParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyRatesAPI.Data
+{
+    public class SearchParameters
+    {
+        public Dictionary<string, string> CurrencyCodes { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
